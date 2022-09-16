ab6e702c2fb2f012e177e4af760b7d04d7ef2750
HoCongViet2501 <viet.forwork.2501@gmail.com>
2022-09-16 21:45:36 +0700

    finish first screen

--- App.js
+++ App.js
@@ -1,11 +1,8 @@
-import { View } from 'react-native';
 import FisrtScreen from './pages/FirstScreen';
 
 export default function App() {
   return (
-    <View >
       <FisrtScreen></FisrtScreen>
-    </View>
   );
 }
 
--- pages/FirstScreen.js
+++ pages/FirstScreen.js
@@ -1,26 +1,65 @@
-import { Box, Button, Flex, Text } from "@react-native-material/core";
-import { Image } from "react-native";
+import { Button, Text } from "@react-native-material/core";
+import { Image, ImageBackground, StyleSheet, View } from "react-native";
 
 export default function FisrtScreen() {
     return (
-        <Flex direction="column">
-            <Box h={30} m={10}>
-                <Image source={require('../image/circle.png')}></Image>
-            </Box>
-            <Box >
-                <Text variant="h4" style={{ backgroundColor: "red" }}>GROW YOUR BUSSINESS</Text>
-            </Box>
-            <Box >
-                <Text>We will help you to grow your business using online server</Text>
-            </Box>
-            <Box style={{ flex: 4, flexDirection: "row" }}>
-                <Button title="Login" color="yellow" tintColor="black" />
-                <Button title="Sign up" color="yellow" tintColor="black" />
-            </Box>
-            <Box >
-                <Text variant="h1">HOW WE WORK</Text>
-            </Box>
-        </Flex>
+        <View style={styles.outer}>
+            <ImageBackground style={styles.backgroup} source={require('../assets/image/background.png')}>
+                <View style={{ flexDirection: 'column', flex: 1 }}>
+                    <View style={styles.logoOuter}>
+                        <Image style={styles.logoImage} source={require('../assets/image/circle.png')} />
+                    </View>
+                    <View style={{ flex: 1, justifyContent: "flex-end", alignItems: 'center' }} >
+                        <Text style={{ paddingBottom: '5%', fontSize: '25', fontWeight: '800', textAlign: "center" }}>
+                            GROW {"\n"} YOUR BUSINESS
+                        </Text>
+                    </View>
+                    <View style={{ flex: 1, justifyContent: "center", }}>
+                        <Text style={{ paddingHorizontal: '5%', fontSize: '15', fontWeight: '600', textAlign: "center" }}>
+                            We will help you to grow your bussiness using online server
+                        </Text>
+                    </View>
+                    <View style={{ flex: 1, flexDirection: 'row', justifyContent: "center", }}>
+                        <View style={{ flex: 1 }}>
+                            <Button
+                                title="LOGIN"
+                                color="yellow"
+                                style={{ marginHorizontal: '18%', paddingVertical: 5 }}
+                            />
+                        </View>
+                        <View style={{ flex: 1 }}>
+                            <Button
+                                title="SIGN UP"
+                                color="yellow"
+                                style={{ marginHorizontal: '18%', paddingVertical: 5 }}
+                            />
+                        </View>
+                    </View>
+                    <View style={{ flex: 1 }}>
+                        <Text style={{ paddingBottom: '5%', fontSize: '25', fontWeight: '800', textAlign: "center" }}>
+                            HOW WE WORK ?
+                        </Text>
+                    </View>
+                </View>
+            </ImageBackground>
+        </View>
     );
 }
-
+const styles = StyleSheet.create({
+    outer: {
+        flex: 1,
+    },
+    backgroup: {
+        flex: 1
+    },
+    logoOuter: {
+        flex: 2,
+        justifyContent: "flex-end",
+        alignItems: 'center',
+    },
+    logoImage: {
+        marginTop: '10%',
+        width: 142,
+        height: 142
+    }
+});
